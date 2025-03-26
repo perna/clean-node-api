6867488410bb120ae7cfdde794a3948f51294f4f
Anderson Pereira <anderson.pereira@yara.com>
2025-03-26 19:18:18 -0300

    chore: update eslint packages and config

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,20 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint'
-import standard from 'ts-standard';
+import standardWithTypeScript from "eslint-config-standard-with-typescript";
 
-export default tseslint.config({
-  ...eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  rules: {
-    'no-console': 'warn',
-    'no-unused-vars': 'error',
-    'eqeqeq': 'error',
-    '@typescript-eslint/no-explicit-any': 'warn',
-  },
-  plugins: {
-    standard: standard
-  },
-  overrides: [
-    {
-      files: ['*.ts'],
-      rules: {
-        'standard/object-curly-even-spacing': [2, 'either'],
-        'standard/array-bracket-even-spacing': [2, 'either'],
-        'standard/computed-property-even-spacing': [2, 'even'],
-        'standard/no-callback-literal': [2, ['cb', 'callback']]
-      },
+export default [
+  {
+    files: ["*.ts", "*.tsx"],
+    plugins: {
+      standardWithTypeScript: standardWithTypeScript,
     },
-  ],
-})
+    rules: {
+      "@typescript-eslint/strict-boolean-expressions": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/comma-spacing": "off",
+      "@typescript-eslint/return-await": "off",
+      "@typescript-eslint/restrict-template-expressions": "off",
+      "@typescript-eslint/no-misused-promises": "off",
+      "@typescript-eslint/no-namespace": "off",
+      "import/export": "off"
+    },
+  },
+];
