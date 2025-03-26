a29bef3d12ab8024cc6132f2ecb899ba9a35ca1d
Anderson Pereira <anderson.pereira@yara.com>
2025-03-26 20:06:29 -0300

    test: ensure SignupController returns 400 if no password confirmation is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -43,4 +43,18 @@ describe('SignUp Controller', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new Error('Missing param: password'))
     })
+
+    test('Should return 400 if no password confirmation is provided', () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                name: 'John Doe',
+                email: 'jdoe@test.com',
+                password: '123456',
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error('Missing param: passwordConfirmation'))
+    })
 })
