bdf843f7c631c2a5711affbfa400524e9f251b9b
Anderson Pereira <anderson.pereira@yara.com>
2025-03-26 17:17:47 -0300

    chore: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint'
+import tsStandard from 'eslint-plugin-standard';
+
+export default tseslint.config({
+  ...eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': 'error',
+    'eqeqeq': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+  plugins: [
+    tsStandard,
+  ],
+  overrides: [
+    {
+      files: ['*.ts'],
+      rules: {
+        'standard/object-curly-even-spacing': [2, 'either'],
+        'standard/array-bracket-even-spacing': [2, 'either'],
+        'standard/computed-property-even-spacing': [2, 'even'],
+        'standard/no-callback-literal': [2, ['cb', 'callback']]
+      },
+    },
+  ],
+})
